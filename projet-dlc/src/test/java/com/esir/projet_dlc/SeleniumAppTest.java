cbc1a412edafe585c9abad2ca75942801086c718
cnabot <clement.nabot@etudiant.univ-rennes1.fr>
2019-02-01 10:44:38 +0100

    update selenium test

--- projet-dlc/src/test/java/com/esir/projet_dlc/SeleniumAppTest.java
+++ projet-dlc/src/test/java/com/esir/projet_dlc/SeleniumAppTest.java
@@ -5,6 +5,7 @@ import junit.framework.TestSuite;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -33,63 +34,22 @@ public class SeleniumAppTest extends TestCase {
 		driver.get(URL + "liste");
 		assertEquals(URL + "liste", driver.getCurrentUrl());
 	}
-/*
-	public void testSearch() {
+
+	/*
+	public void testSearch() throws Exception {
 		driver.get(URL + "liste");
 		WebElement element = driver.findElement(By.name("query"));
         element.sendKeys("test");
-		driver.findElement(By.cssSelector("button:nth-child(2)")).click();
+		driver.findElement(By.cssSelector("i.fa.fa-search")).click();
 		assertEquals(URL + "search", driver.getCurrentUrl());
 	}
 	
-	public void testMoviePage() {
+	public void testMoviePage() throws Exception {
 		driver.get(URL + "liste");
-		driver.findElement(By.id("poster11")).click();
+		driver.findElement(By.cssSelector(".movie:nth-child(10) a")).click();
 		assertEquals(URL + "film?id=11", driver.getCurrentUrl());
-	}
-	
-	public void testNextPage() {
-		driver.get(URL + "liste");
-		driver.findElement(By.cssSelector(".next")).click();
-		assertEquals(URL + "liste?page=2", driver.getCurrentUrl());
-	}
-	
-	public void testSwitchPage() {
-		driver.get(URL + "liste");
-		driver.findElement(By.cssSelector(".page-number:nth-child(6)")).click();
-		assertEquals(URL + "liste?page=5", driver.getCurrentUrl());
-	}
-	
-	public void testAccueilButton() {
-		driver.get(URL + "film?id=4");
-		driver.findElement(By.linkText("Accueil")).click();
-		assertEquals(URL + "liste", driver.getCurrentUrl());
-	}
-	
-	public void testHomeButton() {
-		driver.get(URL + "film?id=4");
-		driver.findElement(By.linkText("Liste des films")).click();
-		assertEquals(URL + "liste", driver.getCurrentUrl());
-	}
-	
-	public void testSiteBanner() {
-		driver.get(URL + "film?id=4");
-		driver.findElement(By.cssSelector(".site-title")).click();
-		assertEquals(URL + "liste", driver.getCurrentUrl());
-	}
-	
-	public void testFindMovieButton() {
-		driver.get(URL + "film?id=4");
-		driver.findElement(By.linkText("Trouver un film")).click();
-		assertEquals(URL + "liste", driver.getCurrentUrl());
-	}
-	
-	public void testAddMovieButton() {
-		driver.get(URL + "film?id=4");
-		driver.findElement(By.linkText("Ajouter un film")).click();
-		assertEquals(URL + "addMovie", driver.getCurrentUrl());
 	}*/
-	
+
 	@After
 	public void tearDown() throws Exception {
 		driver.quit();
